179c60b17bcbbc5510ea972059bb0565cdbbf1f0
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-09-29 18:22:23 +0000

    Use python to display members of company by using for loop and data of objects.

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import json
 import os
 from flask import Flask, render_template
 
@@ -12,7 +13,10 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html", page_title="About")
+    data = []
+    with open("data/compania.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company=data)
 
 
 @app.route("/contact")
