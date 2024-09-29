d07ea080185cc11a11890083a28135eedb64def5
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-09-29 20:23:55 +0000

    Activate form with imputs to back and.

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import json
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -18,9 +18,20 @@ def about():
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
 
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/compania.json", "r" ) as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return render_template("member.html", member=member)      
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        print("Hello")
     return render_template("contact.html", page_title="Contact")
 
 
